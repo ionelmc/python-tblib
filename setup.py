0e950c8c0f4386c190dc6005597d3c7641d4dc65
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-12-31 04:12:54 +0200

    Don't bother with python2.6 ...

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development :: Debuggers',
         'Topic :: Utilities',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: Implementation :: CPython',
