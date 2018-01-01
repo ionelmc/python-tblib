c29b063d292543a5abec8767e7b84ca0dab6d883
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-01 06:02:11 -0800

    Remove unused keyword arguments from setup() in setup.py
    Looks like they were copied from an example.

--- setup.py
+++ setup.py
@@ -59,15 +59,4 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
     ],
-    keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
-    ],
-    install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
-    extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
-    },
 )
