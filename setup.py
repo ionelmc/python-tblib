8d2906c22130fc0b2ed0cf20119995df8e1356ea
Jon Dufresne <jon.dufresne@gmail.com>
2019-10-22 07:00:53 -0700

    Drop support for EOL Python 3.4
    Python 3.4 is end-of-life. It is no longer receiving bug fixes,
    including for security issues. Python 3.4 went EOL on March 18, 2019.
    
    Supported: https://devguide.python.org/#status-of-python-branches
    EOL: https://devguide.python.org/devcycle/#end-of-life-branches
    
    Right now, testing on Python 3.4 fails with the error:
    
        ImportError: Twisted on Python 3 requires Python 3.5 or later.
    
    Rather than spend time maintaining, skipping or fixing this test, remove
    the end-of-life support to reduce testing time and maintenance.
    
    Using pypinfo, we can see the PyPI download statistics for tblib from
    the last month, which show low numbers for Python 3.4.
    
    | python_version | percent | download_count |
    | -------------- | ------: | -------------: |
    | 3.6            |  58.72% |        435,682 |
    | 2.7            |  17.55% |        130,204 |
    | 3.7            |  11.91% |         88,401 |
    | 3.5            |  11.59% |         85,975 |
    | 3.4            |   0.18% |          1,322 |
    | 3.8            |   0.05% |            385 |
    | 2.6            |   0.00% |             28 |
    | 3.3            |   0.00% |              3 |
    | Total          |         |        742,000 |
    
    Python 3.4 users can continue installing older releases of tblib. Using
    python_requires ensures pip users will install a compatible version with
    the need to pin it.

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -72,7 +71,7 @@ setup(
     keywords=[
         'traceback', 'debugging', 'exceptions',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
