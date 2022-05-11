4ddf1f07329d0583b35cbcf49c01dc39461283b3
Ionel Cristian Mărieș <contact@ionelmc.ro>
2022-05-11 14:01:33 +0300

    Also drop python 3.6 since tb_next is readonly and it's not worth maintaining that hack.

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -67,7 +66,7 @@ setup(
     keywords=[
         'traceback', 'debugging', 'exceptions',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
