166bc55d1485544c259a246b24abc1b463381e6c
Tim Maxwell <tmaxwell@anthropic.com>
2023-10-05 16:01:09 -0700

    Add support for various Python 3.11 things

--- tests/test_pickle_exception.py
+++ tests/test_pickle_exception.py
@@ -12,6 +12,7 @@ import pytest
 import tblib.pickling_support
 
 has_python3 = sys.version_info.major >= 3
+has_python311 = sys.version_info >= (3, 11)
 
 
 @pytest.fixture
@@ -33,17 +34,24 @@ def test_install(clear_dispatch_table, how, protocol):
     if how == 'global':
         tblib.pickling_support.install()
     elif how == 'class':
-        tblib.pickling_support.install(CustomError, ZeroDivisionError)
+        tblib.pickling_support.install(CustomError, ValueError, ZeroDivisionError)
 
     try:
         try:
-            1 / 0  # noqa: B018
+            try:
+                1 / 0  # noqa: B018
+            finally:
+                # In Python 3, the ValueError's __context__ will be the ZeroDivisionError
+                raise ValueError("blah")
         except Exception as e:
             # Python 3 only syntax
             # raise CustomError("foo") from e
             new_e = CustomError('foo')
             if has_python3:
                 new_e.__cause__ = e
+                if has_python311:
+                    new_e.add_note("note 1")
+                    new_e.add_note("note 2")
             raise new_e
     except Exception as e:
         exc = e
@@ -54,6 +62,7 @@ def test_install(clear_dispatch_table, how, protocol):
     exc.x = 1
     if has_python3:
         exc.__cause__.x = 2
+        exc.__cause__.__context__.x = 3
 
     if how == 'instance':
         tblib.pickling_support.install(exc)
@@ -65,10 +74,17 @@ def test_install(clear_dispatch_table, how, protocol):
     assert exc.x == 1
     if has_python3:
         assert exc.__traceback__ is not None
-        assert isinstance(exc.__cause__, ZeroDivisionError)
+        assert isinstance(exc.__cause__, ValueError)
         assert exc.__cause__.__traceback__ is not None
         assert exc.__cause__.x == 2
         assert exc.__cause__.__cause__ is None
+        if has_python311:
+            assert exc.__notes__ == ["note 1", "note 2"]
+            assert isinstance(exc.__cause__.__context__, ZeroDivisionError)
+            assert exc.__cause__.__context__.x == 3
+            assert exc.__cause__.__context__.__cause__ is None
+            assert exc.__cause__.__context__.__context__ is None
+
 
 
 @tblib.pickling_support.install
@@ -88,6 +104,24 @@ def test_install_decorator():
     assert exc.x == 1
     if has_python3:
         assert exc.__traceback__ is not None
+
+
+@pytest.mark.skipif(sys.version_info < (3,11), reason="no BaseExceptionGroup before Python 3.11")
+def test_install_instance_recursively(clear_dispatch_table):
+    exc = BaseExceptionGroup(
+        "test",
+        [
+            ValueError("foo"),
+            CustomError("bar")
+        ]
+    )
+    exc.exceptions[0].__cause__ = ZeroDivisionError("baz")
+    exc.exceptions[0].__cause__.__context__ = AttributeError("quux")
+
+    tblib.pickling_support.install(exc)
+
+    installed = set(c for c in copyreg.dispatch_table if issubclass(c, BaseException))
+    assert installed == {ExceptionGroup, ValueError, CustomError, ZeroDivisionError, AttributeError}
 
 
 @pytest.mark.skipif(sys.version_info[0] < 3, reason='No checks done in Python 2')
